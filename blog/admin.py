f7c9f2b48c98a1f06796eaf77f8bcbfabc4aea7c
kosyachenko <kosyachenko@gmail.com>
2019-12-29 23:11:27 +0200

    attampt to correct admin

--- blog/admin.py
+++ blog/admin.py
@@ -2,6 +2,16 @@ from django.contrib import admin
 from .models import Post, Comment
 
 # Register your models here.
-admin.site.register(Post)
+#admin.site.register(Post)
 admin.site.register(Comment)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_date')
+    list_filter = ('published_date', 'created_date', 'author')
+    search_fields = ('title', 'body')
+    #prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author',)
+    date_hierarchy = 'publish'
+    ordering = ('published_date')
 
